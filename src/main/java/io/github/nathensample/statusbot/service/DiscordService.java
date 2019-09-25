972ba08c1a10c35cf3cbdaf233e844bb222eba6a
Nathen <nathen.sample@gmail.com>
2019-09-25 19:17:32 +0100

    remove unused import

--- src/main/java/io/github/nathensample/statusbot/service/DiscordService.java
+++ src/main/java/io/github/nathensample/statusbot/service/DiscordService.java
@@ -11,7 +11,6 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
