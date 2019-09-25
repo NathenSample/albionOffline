d54b3277c77b7e3dc3aa42aea380e59cc6bc15fc
Nathen <nathen.sample@gmail.com>
2019-09-25 19:39:17 +0100

    Can now be started during DT

--- src/main/java/io/github/nathensample/statusbot/Application.java
+++ src/main/java/io/github/nathensample/statusbot/Application.java
@@ -9,13 +9,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class Application
 {
 	//TODO: Properly class/method link the javadoc
-	//TODO: Crashes if started during DT,
-	/*
-	Error starting ApplicationContext. To display the auto-configuration report re-run your application with 'debug' enabled.
-2019-09-23 11:55:50.849 ERROR 22191 --- [pool-1-thread-1] o.s.s.s.TaskUtils$LoggingErrorHandler    : Unexpected error occurred in scheduled task.
-
-java.io.IOException: Server returned HTTP response code: 500 for URL: http://serverstatus.albiononline.com
-	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
